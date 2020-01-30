c5ed4732aae2cdce7f4ad48c387f59e24f35044d
torla <munford.ns@gmail.com>
2020-01-30 11:53:04 +0100

    retry event

--- IdeaSim/Test.py
+++ IdeaSim/Test.py
@@ -1,10 +1,16 @@
 from IdeaSim.Event import Event
+from IdeaSim.Manager import Manager
 from IdeaSim.Simulation import Simulation
 
 
 def test(event):
+
+    if event.sim.now % 120 != 0:
+        event.sim.manager.activate()
+        raise Manager.RetryLater
     event.sim.logger.log(str(event))
     Event(event.sim, event.sim.now + 1, "prova")
+
 
 if __name__ == '__main__':
     sim = Simulation()
