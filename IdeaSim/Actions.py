c4c69655b679250226e579ef78cbe8dd5ec2c9ea
torla <munford.ns@gmail.com>
2020-01-30 12:23:07 +0100

    perfermor and execution (need testing)

---
+++ IdeaSim/Actions.py
@@ -0,0 +1,122 @@
+import itertools
+from enum import Enum
+
+from simpy import Container
+
+from IdeaSim import Simulation
+from IdeaSim.Resources import Performer
+
+
+class Action:
+    new_id = itertools.count()
+
+    def __init__(self, action_graph, action_type, who, param=None, after=None):
+        self.id = next(self.new_id)
+        assert isinstance(action_graph, ActionsGraph)
+        action_graph.actions[self.id] = self
+        self.actionType = action_type
+        self.who = who
+        assert isinstance(param, dict) or param is None
+        self.param = {} if param is None else param
+        assert isinstance(after, list) or after is None
+        self.after = [] if after is None else after
+
+
+class Block(Action):
+
+    def __init__(self, action_graph, action_type, who, param=None, after=None):
+        super().__init__(action_graph, action_type, who, param, after)
+
+
+class Free(Action):
+
+    def __init__(self, action_graph, action_type, who, param=None, after=None):
+        super().__init__(action_graph, action_type, who, param, after)
+
+
+class ActionsGraph:
+    def __init__(self, sim):
+        assert isinstance(sim, Simulation.Simulation)
+        self.sim = sim
+        self.actions = {}
+
+
+class Executor:
+    def __init__(self, sim, action_tree):
+
+        assert isinstance(action_tree, ActionsGraph)
+        self.action_tree = action_tree
+        self.sim = sim
+        sim.process(self.run())
+
+    def run(self):
+        wait = None
+        taken_inf = []
+        completed_flags = {}
+
+        for action_id in self.action_tree.actions.keys():
+            completed_flags[action_id] = Container(self.sim)
+
+        for action in self.action_tree.actions.values():
+            self.sim.process(
+                self.execute(action, taken_inf, self.sim, completed_flags))
+
+        for flag in completed_flags.values():
+            if wait is None:
+                wait = flag.get(1)
+            else:
+                wait = wait & flag.get(1)
+        yield wait
+
+        if len(taken_inf) != 0:
+            raise Exception("Resources not free at end of task")
+
+        self.sim.manager.activate()
+
+    def execute(self, action, taken_inf, sim, completed_flags):
+        assert isinstance(action, Action)
+        assert isinstance(sim, Simulation.Simulation)
+        wait = None
+        try:
+            for action_id in action.after:
+                if wait is None:
+                    wait = completed_flags[action_id].get(1)
+                else:
+                    wait = wait & completed_flags[action_id].get(1)
+            if wait is not None:
+                yield wait
+        except KeyError:
+            sim.logger.log("waiting for non existing action", 0, sim.logger.Type.FAIL)
+
+        if isinstance(action, Block):
+            yield sim.get_res_by_id(action.who)
+            taken_inf.append(sim.find_res_by_id(action.who))
+            completed_flags[action.id].put(float('inf'))
+            sim.logger.log("blocking  " + str(
+                sim.find_res_by_id(action.who)),
+                           7)
+
+        elif isinstance(action, Free):
+            inf = list(filter(lambda x: x.id == action.who, taken_inf))[0]
+            yield sim.put_res(inf)
+            taken_inf.remove(inf)
+            completed_flags[action.id].put(float('inf'))
+            # manager is activated after anything is free
+            sim.logger.log("Free " + str(
+                sim.find_res_by_id(action.who)
+            ),
+                           7)
+            self.sim.manager.activate()
+
+        else:
+            try:
+                yield self.sim.process(
+                    list(filter(lambda x: x.id == action.who, taken_inf))[0].perform(action, taken_inf, sim))
+            except Performer.IllegalAction as err:
+                sim.logger.log(str(err), type=sim.logger.Type.FAIL)
+            except KeyError as err:
+                sim.logger.log("Action parameter not defined" + str(err), type=sim.logger.Type.FAIL)
+            except IndexError as err:
+                sim.logger.log("Performer not blocked " + str(err), type=sim.logger.Type.FAIL)
+
+            completed_flags[action.id].put(float('inf'))
--- IdeaSim/Resources.py
+++ IdeaSim/Resources.py
@@ -2,6 +2,8 @@ import itertools
 
 from simpy import FilterStore
 
+from IdeaSim import Simulation
+
 
 class Resources(FilterStore):
     def __init__(self, env, capacity=float('inf')):
@@ -11,7 +13,20 @@ class Resources(FilterStore):
 class Resource:
     new_id = itertools.count()
 
-    def __init__(self, position, parameter):
-        self.parameter = parameter
-        self.position = position
+    def __init__(self, sim):
+        assert isinstance(sim, Simulation.Simulation)
+        self.sim = sim
         self.id = next(self.new_id)
+
+
+class Performer(Resource):
+    def __init__(self, sim):
+        super().__init__(sim)
+        self.action_map = {}
+
+    class IllegalAction(Exception):
+        def __init__(self, msg):
+            self.msg = msg
+
+    def perform(self, action):
+        self.action_map[action.actionType](action)
